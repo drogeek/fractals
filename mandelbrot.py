bd4aa936b407e597ce56b51ab0a7df26b1e935f9
toor <guiennet.jonathan@gmail.com>
2019-04-05 22:59:29 +0200

    adding animation

--- mandelbrot.py
+++ mandelbrot.py
@@ -2,23 +2,58 @@ import numpy as np
 import matplotlib.pyplot as plt
 from itertools import product
 
-N=10**3
-limit=10**3
+N=10**4
+limit=10**2
 
 real_part=np.linspace(-2,2,N)
 imag_part=np.linspace(0,2,N/2)
 result = []
-for u,v in product(real_part,imag_part):
-    z0=complex(0)
-    for k in range(limit):
-        if abs(z0) <= 2:
-            z0=z0**2+complex(u,v)
-        else:
-            break
-    if k == limit-1:
-        result.append((u,v))
-        result.append((u,-v))
+fig,ax = plt.subplots()
+line, = ax.plot([],[])
+ax.grid()
+xdata, ydata = [], []
 
-print('preparing to plot')
-plt.plot(*zip(*result),',')
+def data_gen(t=0):
+    for u,v in product(real_part,imag_part):
+        z0=complex(t)
+        for k in range(limit):
+            if abs(z0) <= 2:
+                z0=z0**2+complex(u,v)
+            else:
+                break
+        if k == limit-1:
+            yield u,v
+
+def init():
+    ax.set_ylim(-0.1,0.1)
+    ax.set_xlim(-2,-1.8)
+    del xdata[:]
+    del ydata[:]
+    line.set_data(xdata,ydata)
+    return line,
+
+def run(data):
+    u,v = data
+    xdata.append(u)
+    xdata.append(u)
+    ydata.append(v)
+    ydata.append(-v)
+    line.set_data(xdata,ydata)
+    xmin, xmax = ax.get_xlim()
+    ymin, ymax = ax.get_ylim()
+
+    if u >= xmax:
+        if xmax < 0:
+            ax.set_xlim(xmin,xmax/1.5)
+        else:
+            ax.set_xlim(xmin,xmax*1.5)
+        ax.figure.canvas.draw()
+    if v >= ymax:
+        ax.set_ylim(ymin-0.1,ymax+0.1)
+        ax.figure.canvas.draw()
+    line.set_data(xdata,ydata)
+    return line,
+
+import matplotlib.animation as animation
+ani = animation.FuncAnimation(fig, run, data_gen, blit=False, interval = 1, repeat = False, init_func=init)
 plt.show()
