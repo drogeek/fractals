bf979d2296bbcdee51909061bf287e4787564c85
toor <guiennet.jonathan@gmail.com>
2019-04-06 12:33:01 +0200

    adding random plotting, allowing you to zoom in

--- mandelbrot.py
+++ mandelbrot.py
@@ -5,17 +5,16 @@ from itertools import product
 N=10**4
 limit=10**2
 
-real_part=np.linspace(-2,2,N)
-imag_part=np.linspace(0,2,N/2)
-result = []
-fig,ax = plt.subplots()
-line, = ax.plot([],[])
-ax.grid()
 xdata, ydata = [], []
+fig, ax = plt.subplots()
+ax.grid()
+ln, = plt.plot([],[],',')
 
-def data_gen(t=0):
-    for u,v in product(real_part,imag_part):
-        z0=complex(t)
+def data_gen():
+    while True:
+        u=np.random.uniform(*ax.get_xlim())
+        v=np.random.uniform(*ax.get_ylim())
+        z0=complex(0)
         for k in range(limit):
             if abs(z0) <= 2:
                 z0=z0**2+complex(u,v)
@@ -25,34 +24,21 @@ def data_gen(t=0):
             yield u,v
 
 def init():
-    ax.set_ylim(-0.1,0.1)
-    ax.set_xlim(-2,-1.8)
+    ax.set_ylim(-2,2)
+    ax.set_xlim(-2,2)
     del xdata[:]
     del ydata[:]
-    line.set_data(xdata,ydata)
-    return line,
+    ln.set_data(xdata,ydata)
+    return ln,
 
 def run(data):
     u,v = data
     xdata.append(u)
-    xdata.append(u)
     ydata.append(v)
+    xdata.append(u)
     ydata.append(-v)
-    line.set_data(xdata,ydata)
-    xmin, xmax = ax.get_xlim()
-    ymin, ymax = ax.get_ylim()
-
-    if u >= xmax:
-        if xmax < 0:
-            ax.set_xlim(xmin,xmax/1.5)
-        else:
-            ax.set_xlim(xmin,xmax*1.5)
-        ax.figure.canvas.draw()
-    if v >= ymax:
-        ax.set_ylim(ymin-0.1,ymax+0.1)
-        ax.figure.canvas.draw()
-    line.set_data(xdata,ydata)
-    return line,
+    ln.set_data(xdata,ydata)
+    return ln,
 
 import matplotlib.animation as animation
 ani = animation.FuncAnimation(fig, run, data_gen, blit=False, interval = 1, repeat = False, init_func=init)
