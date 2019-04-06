4c2583d0404595a6ee2c12026b673cd4ea184c2c
toor <guiennet.jonathan@gmail.com>
2019-04-06 18:35:14 +0200

    add colors, and avoid square root computing

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,44 +1,49 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from itertools import product
+import matplotlib.cm as cmx
+import matplotlib.colors as colors
 
-N=10**4
-limit=10**2
+limit=20
 
 xdata, ydata = [], []
 fig, ax = plt.subplots()
 ax.grid()
-ln, = plt.plot([],[],',')
+normalize = colors.Normalize(0,limit)
+cm = plt.get_cmap('plasma')
+scalarMap = cmx.ScalarMappable(norm=normalize, cmap=cm)
 
+squared_modulus = lambda x : x.real*x.real + x.imag*x.imag
 def data_gen():
     while True:
         u=np.random.uniform(*ax.get_xlim())
         v=np.random.uniform(*ax.get_ylim())
         z0=complex(0)
         for k in range(limit):
-            if abs(z0) <= 2:
+            if squared_modulus(z0) <= 4:
                 z0=z0**2+complex(u,v)
             else:
                 break
         if k == limit-1:
-            yield u,v
+            color = -1
+        else:
+            color = k
+        yield u,v,color
+
 
 def init():
     ax.set_ylim(-2,2)
     ax.set_xlim(-2,2)
-    del xdata[:]
-    del ydata[:]
-    ln.set_data(xdata,ydata)
-    return ln,
 
 def run(data):
-    u,v = data
-    xdata.append(u)
-    ydata.append(v)
-    xdata.append(u)
-    ydata.append(-v)
-    ln.set_data(xdata,ydata)
-    return ln,
+    u,v,c = data
+    if c==-1:
+        result=ax.scatter(u,v,s=1,c='black')
+        result2=ax.scatter(u,-v,s=1,c='black')
+    else:
+        result=ax.scatter(u,v,s=1,c=scalarMap.to_rgba(c))
+        result2=ax.scatter(u,-v,s=1,c=scalarMap.to_rgba(c))
+    return result,result2
 
 import matplotlib.animation as animation
 ani = animation.FuncAnimation(fig, run, data_gen, blit=False, interval = 1, repeat = False, init_func=init)
